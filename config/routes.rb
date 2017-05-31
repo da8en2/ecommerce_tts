bd6f74407cb9996322e2753c6c43b2605a6ec69c
Daniel Endres <danielendres@gmail.com>
2017-05-31 09:28:15 -0700

    added cart functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  get 'view_order' => 'cart#view_order'
+
+  get 'checkout' => 'cart#checkout'
+
   devise_for :users
+
   root 'storefront#all_items'
 
   get 'all_items' => 'storefront#all_items'
 
-  get 'categorical' => 'storefront#items_by_category'
+  get 'categorial' => 'storefront#items_by_category'
 
   get 'branding' => 'storefront#items_by_brand'
 
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
