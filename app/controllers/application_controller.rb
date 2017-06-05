7874479397515de8b38e1f369aa1bc5688e3380e
Daniel Endres <danielendres@gmail.com>
2017-06-05 08:50:21 -0700

    users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   before_action :categories, :brands
 
@@ -11,6 +11,15 @@ class ApplicationController < ActionController::Base
 
   def brands
     @brands = Product.pluck(:brand).sort.uniq
+  end
+
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password,
+       :password_confirmation, :role) }
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password,
+       :password_confirmation, :current_password, :role) }
   end
 
 end
